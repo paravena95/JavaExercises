ec94f5a419263b3ac9cc79f44e4d323b3e816e3d
pabloaravena <p.aravenafe@gmail.com>
2022-11-01 16:42:12 -0300

    Ejercicio Tema 8 Java - OpenBoot Camp Practicando encapsulamiento

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Esto es una persona");
+        Persona p = new Persona();
+
+        p.setEdad(27);
+        p.setNombre("Pablo Aravena");
+        p.setTelefono("+56912345678");
+
+        System.out.println("La persona se llama: "+p.getNombre()+", tiene "+p.getEdad()+" años y su teléfono es: "+p.getTelefono());
+    }
+}
+
+class Persona{
+    int edad;
+    String telefono;
+    String nombre;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
